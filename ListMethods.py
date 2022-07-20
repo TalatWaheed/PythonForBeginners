f044f7517ca9a95fb5681c89266325c2e600f6c0
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-20 09:08:56 +0530

    append(),insert(),extend(),count() List Methods

---
+++ ListMethods.py
@@ -0,0 +1,30 @@
+#append() method
+#Add single item at end of the list
+
+newlist = [3, "Hey", 8, "My",8.6]
+newlist.append("You")                       #It will add "You" item at end of the list
+print(newlist)                                                                        
+
+
+#insert() method
+#It is used to add elements at the desired position, It requires two  arguments (position,value)
+
+list = [1, ”You”,  8.9, ”Too”]
+list.insert(3, 14)                          #Add new element 14 at position 3(index 3) in the list
+print(list)
+
+
+#extend() method
+#You can add multiple elements at the same time at the end of the list with extend() method
+
+list = [1, ”You”,  8.9, ”Too”]
+list.extend(“Talat”, 1, 9.7)                 #Add multiple new elements like “Talat” , 1 , 9.7 at end of the list
+print(list)
+
+
+#count() method
+#It returns the count of how many times a given object occurs in a List.
+
+Num = [1, 4, 2, 9, 7, 8, 9, 3, 1]            #It will count how many times 9 occur in the list 
+x = Num.count(9)
+
