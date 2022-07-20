3c1b385515217dde9d756c33efd3ac59b5c0bad9
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-20 08:52:12 +0530

    Change one DataType to Another DataType

---
+++ TypeConversion.py
@@ -0,0 +1,28 @@
+#Type Conversion of Datatypes (Convert one datatype to another datatype)
+
+a = 6
+o = float(a)    #convert from int to float
+
+print(o)
+print(type(o))
+
+print("**********")
+
+n = 9.8
+#convert from float to int
+
+y = int(n)
+
+print(y)
+print(type(y))
+
+print("**********")
+
+l = 9
+
+#convert from int to complex
+
+w = complex(l)
+
+print(w)
+print(type(w))
