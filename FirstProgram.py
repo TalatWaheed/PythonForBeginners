8ef3f3ce1c8abf47c983d61ceb8e2393b2126ccd
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-20 08:45:10 +0530

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,15 @@
+# Sum Of Two Numbers
+
+'''First Define Variable
+these are pre defined variables '''
+
+a = 2
+b = 4
+
+#Store sum of two Numbers in c 
+
+c = a + b                             
+
+#It will print 'c'  Which is Sum of two numbers
+  
+print(c) 
