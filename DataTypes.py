5530daaf5f95f7772f2e97a0fde23f14298c5373
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-20 08:48:16 +0530

    DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,14 @@
+string = "This is python program"                   #String
+print(string)
+
+print("********************")
+
+
+list = [1, "I", 3, "Am" ,8, "Talat"]                #List
+print(list)
+
+print("********************")
+
+
+tuple = ("Hi",5, "there",8)                          #Tuple
+print(tuple)
