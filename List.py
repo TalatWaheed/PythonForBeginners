deb85eef24de35d7be7e504773c63cfaf08ad1e6
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-20 08:58:54 +0530

    List and Positive,Negative Indexing

---
+++ List.py
@@ -0,0 +1,20 @@
+#List items are ordered and  changeable,List are indexed
+
+list = ["Hey" , "You" ,"There"]    #string
+print(list)
+
+list1 = [1,"Talat" ,7.8, True ]    #Int,String,Float ,Bool
+print(list1)
+
+
+#Positive Indexing
+#Start with 0 and move left to right
+
+mylist = [3,"Hey" ,"My" ,8.6]
+#index    0   1     2     3
+
+#Negative Indexing
+#Start with -1 and move right to left
+
+mylist = [3,"Hey" ,"My" ,8.6]
+#negative -4  -3    -2     -1
