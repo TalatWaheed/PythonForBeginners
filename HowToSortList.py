8f4cdd129963da2a56728f9b2ec4000261266aaf
Talat Waheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-22 09:10:11 +0530

    Create HowToSortList.py

---
+++ HowToSortList.py
@@ -0,0 +1,6 @@
+#You can sort the list with sort() method
+#By default Sort the list in ascending order, You can also sort list in descending order and user-defined order
+
+list = [1, 7, 8, 2, 10, 5]
+list.sort()                    #sort the list in ascending order
+print(list)
