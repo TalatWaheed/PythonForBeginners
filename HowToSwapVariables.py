f983a18d975595ec1e6d2445b6105271b89dc6fd
Talat Waheed <35964435+TalatWaheed@users.noreply.github.com>
2022-07-22 09:15:05 +0530

    SwapVariablesInPython

---
+++ HowToSwapVariables.py
@@ -0,0 +1,21 @@
+#You can swap variables with temp variable (temp is temporary variables which is used to store value of variable)
+
+#(1) with temp
+
+a = 4
+b = 9
+
+print(a)
+print(b)
+
+#swap variables with temp
+
+temp = a
+a = b
+b = temp
+
+print("After swapping variables")
+print(a)
+print(b)
+
+#(2) Without temp
