01446dfeb7e1562a0af143e66bb6c77f3c6919c4
Talat Waheed <35964435+TalatWaheed@users.noreply.github.com>
2022-08-06 11:43:17 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+#Tuples are used to store multiple items in a single variable.
+#Tuple is ordered and unchangeable
+
+#Syntax
+#varname = (element1,element2,element3)
+
+#For Example
+tuple = (1, 6,  8, 2, 10)
+print(tuple)
