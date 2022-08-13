07029d64cf83066deaceb623c62793d081c7bca5
Talat Waheed <35964435+TalatWaheed@users.noreply.github.com>
2022-08-13 12:02:52 +0530

    PythonSet

---
+++ PythonSet.py
@@ -0,0 +1,13 @@
+#Python Set
+#A set is an unordered, unique, immutable (cannot be changed) collection of items
+
+#You can use same data type elements in the Set
+myset = {"Orange","Red","Yellow","Green"}
+print(myset)
+
+set = {1,3,5,9}
+print(set)
+
+#You can use different data type in Set
+set = {1,3,5,9,"Hey",7.9}
+print(set)
